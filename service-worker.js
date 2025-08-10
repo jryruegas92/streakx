d71402f4765baa0297b44bd6a240ea4f58951787
jryruegas92 <rey.yruegas@berkeley.edu>
2025-08-09 22:00:58 -0700

    Initial PWA uploadInitial PWA uploadInitial commit with PWA files

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE = 'streakx-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null)))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(
+      caches.match(e.request).then(cached => cached || fetch(e.request))
+    );
+  }
+});

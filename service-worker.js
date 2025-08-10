890aef7d147eeeaf756a487f727215995c18c48b
jryruegas92 <rey.yruegas@berkeley.edu>
2025-08-09 22:28:00 -0700

    V2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'streakx-v1';
+const CACHE = 'streakx-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -6,22 +6,15 @@ const ASSETS = [
   './icons/icon-192.png',
   './icons/icon-512.png'
 ];
-
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
 });
-
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null)))
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null))));
 });
-
 self.addEventListener('fetch', (e) => {
   const url = new URL(e.request.url);
   if (url.origin === location.origin) {
-    e.respondWith(
-      caches.match(e.request).then(cached => cached || fetch(e.request))
-    );
+    e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
   }
 });
